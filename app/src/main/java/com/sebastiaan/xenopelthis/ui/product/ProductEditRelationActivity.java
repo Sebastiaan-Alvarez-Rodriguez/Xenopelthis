712731e615e4c126b864b9dc69f157fb8755af2f
Sebastiaan Alvarez Rodriguez <sebastiaanalva@gmail.com>
2019-07-22 19:21:51 +0200

    Merge Barcode into master

--- app/src/main/java/com/sebastiaan/xenopelthis/ui/product/ProductEditRelationActivity.java
+++ app/src/main/java/com/sebastiaan/xenopelthis/ui/product/ProductEditRelationActivity.java
@@ -4,7 +4,6 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
