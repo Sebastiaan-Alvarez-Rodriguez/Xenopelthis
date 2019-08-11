bd2aa58431a606ce9577383d771339354a03d6a9
Sebastiaan Alvarez Rodriguez <sebastiaanalva@gmail.com>
2019-08-11 12:36:51 +0200

    Added documentation and removed unnecesary files

--- app/src/main/java/com/sebastiaan/xenopelthis/camera/Camera.java
+++ app/src/main/java/com/sebastiaan/xenopelthis/camera/Camera.java
@@ -19,9 +19,10 @@ public class Camera {
     private static final String TAG = "AndroidCameraApi";
 
     /**
-     * Function to open a logical camera, and create a preview stream usecase
+     * Function to open a logical camera, and create a preview stream use case
      * @param textureView The textureview to receive previews
-     * @param preferredResolution  the preferred rendering resolution (may differ depending on device capabilities)
+     * @param preferredResolution  The resolution of stream frames. The actual resolution may differ,
+     *                             because of hardware capabilities
      * @throws SecurityException when we tried to access the hardware camera, while we did not have user permission
      */
     public Preview render(@NonNull TextureView textureView, @NonNull Size preferredResolution) throws SecurityException {
@@ -34,10 +35,21 @@ public class Camera {
         return preview;
     }
 
+    /**
+     * @see #render(TextureView, Size)
+     * Works just like this method, with fixed preferredResolution 800 x 600
+     */
     public Preview render(@NonNull TextureView textureView) throws SecurityException {
         return this.render(textureView, new Size(800,600));
     }
 
+    /**
+     * Creates an analysis stream use case (stream of YUV_420_888)
+     * @param analyzer The analyzer which tells what to do with each analysis frame
+     * @param preferredResolution The resolution of stream frames. The actual resolution may differ,
+     *                            because of hardware capabilities
+     * @return an ImageAnalysis use case
+     */
     public ImageAnalysis analyse(@NonNull ImageAnalysis.Analyzer analyzer, @NonNull Size preferredResolution) {
         ImageAnalysisConfig imageAnalysisConfig = new ImageAnalysisConfig.Builder()
                 .setTargetResolution(preferredResolution)
@@ -51,32 +63,11 @@ public class Camera {
         return imageAnalysis;
     }
 
+    /**
+     * @see #analyse(ImageAnalysis.Analyzer, Size)
+     * Works just like this method, with fixed preferredResolution 800 x 600
+     */
     public ImageAnalysis analyse(@NonNull ImageAnalysis.Analyzer analyzer) {
         return this.analyse(analyzer, new Size(800,600));
     }
-
-//    /**
-//     * Gets the orientation in which the picture should be taken
-//     * (such that we always display image always aligned with Earth gravity center)
-//     * @param deviceOrientation The rotation of the screen. In actvities: getWindowManager().getDefaultDisplay().getRotation();
-//     * @return amount of degrees to turn image
-//     * @throws CameraAccessException when we could not access the hardware device to getLive sensor data from
-//     */
-//    @SuppressWarnings("ConstantConditions")
-//    private int getJpegOrientation(int deviceOrientation) throws CameraAccessException {
-//        CameraCharacteristics hardwareCharacterstics = manager.getCameraCharacteristics(device.getId());
-//
-//        if (deviceOrientation == android.view.OrientationEventListener.ORIENTATION_UNKNOWN)
-//            return 0;
-//        int sensorOrientation = 0;
-//        if (hardwareCharacterstics.get(CameraCharacteristics.SENSOR_ORIENTATION) != null)
-//            sensorOrientation = hardwareCharacterstics.get(CameraCharacteristics.SENSOR_ORIENTATION);
-//
-//        // Round device orientation to a multiple of 90
-//        deviceOrientation = (deviceOrientation + 45) / 90 * 90;
-//
-//        // Calculate desired JPEG orientation relative to camera orientation to make
-//        // the image upright relative to the device orientation
-//        return (sensorOrientation + deviceOrientation + 360) % 360;
-//    }
 }
--- app/src/main/java/com/sebastiaan/xenopelthis/recognition/Recognitron.java
+++ app/src/main/java/com/sebastiaan/xenopelthis/recognition/Recognitron.java
@@ -3,10 +3,8 @@ package com.sebastiaan.xenopelthis.recognition;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.SurfaceTexture;
 import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.TextureView;
 import android.widget.Toast;
 
@@ -17,11 +15,9 @@ import androidx.camera.core.CameraX;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.Preview;
 import androidx.core.app.ActivityCompat;
-import androidx.lifecycle.LifecycleOwner;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
@@ -29,19 +25,19 @@ import com.sebastiaan.xenopelthis.R;
 import com.sebastiaan.xenopelthis.camera.Camera;
 
 
+/**
+ * Activity to scan for barcodes.
+ * On success, sets RESULT_OK flag and puts raw barcode value in returned intent, under key 'barcode'
+ */
 //  search: android camera2 api tutorial simple
 //  used: https://inducesmile.com/android/android-camera2-api-example-tutorial/
 //  promising: http://coderzpassion.com/android-working-camera2-api/
 // For fix slowness: https://android.jlelse.eu/the-least-you-can-do-with-camera2-api-2971c8c81b8b
 public class Recognitron extends AppCompatActivity {
-    private static final String TAG = "AndroidCameraApi";
     private static final int REQUEST_CAMERA_PERMISSION = 1;
 
     private TextureView textureView;
-
     private Camera camera;
-
-    private static final int DELAY_CAPTURE_MILLISECONDS = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,64 +45,67 @@ public class Recognitron extends AppCompatActivity {
         setContentView(R.layout.activity_recognitron);
         FirebaseApp.initializeApp(getApplicationContext());
         findGlobalViews();
-        if (!checkPermission())
+        if (!checkPermission()) {
             askPermission();
-        else
-//            prepareTexture();
+        } else {
             openCamera();
-            CameraX.bindToLifecycle((LifecycleOwner) this,
+            CameraX.bindToLifecycle(this,
                     render(),
                     analyse());
+        }
     }
 
+    /**
+     * Locates necessary member views in the view tree
+     */
     private void findGlobalViews() {
         textureView = findViewById(R.id.recognitron_cameraview);
     }
 
-    private void prepareTexture() {
-        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
-            @Override
-            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-                //open your camera here
-
-            }
-            @Override
-            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-                // Transform you image captured size according to the surface width and height
-            }
-            @Override
-            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-                return false;
-            }
-            @Override
-            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-            }
-        });
-    }
-
+    /**
+     * Checks whether this app has permission to use the camera
+     * @return true if this app has camera permission, otherwise false
+     */
     private boolean checkPermission() {
         return (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
     }
 
+    /**
+     * Asks permission to use the camera
+     */
     private void askPermission() {
         ActivityCompat.requestPermissions(Recognitron.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
     }
 
-
+    /**
+     * Constructs a camera object to use
+     */
     private void openCamera() {
-        Log.e(TAG, "is camera open");
         camera = new Camera();
     }
 
+    /**
+     * Constructs a Preview use case (to render)
+     * @return the Preview use case to render
+     */
     private Preview render() {
         return camera.render(textureView);
     }
 
+    /**
+     * Constructs an ImageAnalysis use case, to analyse frames (YUV_420_888 type)
+     * @return the ImageAnalysis use case
+     */
     private ImageAnalysis analyse() {
         return camera.analyse((image, rotationDegrees) -> interpretResult(image.getImage(), rotationDegrees));
         //TODO: copy-construct image to reduce load on imaging pipeline?
     }
 
+    /**
+     * Tries to read a barcode. Exits the activity on success (and sets RESULT_OK flag)
+     * @param picture the Image to be scanned for barcodes.
+     * @param rotation the rotation, in [0-360), which tells by how many degrees the image should be rotated to match reality
+     */
     private void interpretResult(@Nullable Image picture, @FirebaseVisionImageMetadata.Rotation int rotation) {
         if (picture == null)
             return;
@@ -115,21 +114,12 @@ public class Recognitron extends AppCompatActivity {
         FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
         detector.detectInImage(image)
                 .addOnSuccessListener(firebaseVisionBarcodes -> {
-                    for (FirebaseVisionBarcode barcode : firebaseVisionBarcodes) {
-                        Log.e("BAR_RECOGNISED", "Got something!!!!!!!");
-                        Log.e("BAR_RECOGNISED", "Raw: "+ barcode.getRawValue());
-                        Log.e("BAR_RECOGNISED", "Display: "+barcode.getDisplayValue());
-                        Log.e("BAR_RECOGNISED", "Format: "+barcode.getFormat());
-                    }
                     if (!firebaseVisionBarcodes.isEmpty()) {
                         Intent intent = new Intent();
                         intent.putExtra("barcode", firebaseVisionBarcodes.get(0).getDisplayValue());
                         setResult(RESULT_OK, intent);
                         finish();
                     }
-                })
-                .addOnFailureListener(e -> {
-                    Log.e("BAR_FAILED", "Failure: "+e.getMessage());
                 });
     }
 
@@ -137,7 +127,7 @@ public class Recognitron extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == REQUEST_CAMERA_PERMISSION) {
             if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
-                Toast.makeText(Recognitron.this, "Sorry!!!, you can't scan without granting camera permission", Toast.LENGTH_LONG).show();
+                Toast.makeText(Recognitron.this, "You can't scan without granting camera permission", Toast.LENGTH_LONG).show();
                 finish();
             }
         }
