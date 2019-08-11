54ca202ad1022d0ff38a898c4d526f0ceafcd9e0
Sebastiaan Alvarez Rodriguez <sebastiaanalva@gmail.com>
2019-08-11 14:26:02 +0200

    Created main menu and settings activity

--- app/src/main/java/com/sebastiaan/xenopelthis/ui/main/MainActivity.java
+++ app/src/main/java/com/sebastiaan/xenopelthis/ui/main/MainActivity.java
@@ -1,12 +1,20 @@
 package com.sebastiaan.xenopelthis.ui.main;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import com.google.android.material.tabs.TabLayout;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 import com.sebastiaan.xenopelthis.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,9 +26,45 @@ public class MainActivity extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
+        TabLayout tabs = findViewById(R.id.main_tabs);
         tabs.setupWithViewPager(viewPager);
+        setupActionBar();
     }
+
+    private void setupActionBar() {
+        Toolbar myToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setTitle("Xenopelthis");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.main_menu_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.main_menu_settings);
+        Drawable icon = item.getIcon();
+        icon.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+
+        item.setIcon(icon);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
