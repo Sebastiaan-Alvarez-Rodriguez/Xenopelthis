7bbbcb9483c856b10812a2f79ed3d19b033d7d85
MariskaIJpelaar <m.m.j.ijpelaar@gmail.com>
2019-10-12 19:11:56 +0200

    Settings now display the current language. Next step is for the user to be able to change the language

--- app/src/main/java/com/sebastiaan/xenopelthis/ui/main/activity/settings/SettingsActivity.java
+++ app/src/main/java/com/sebastiaan/xenopelthis/ui/main/activity/settings/SettingsActivity.java
@@ -3,51 +3,48 @@ package com.sebastiaan.xenopelthis.ui.main.activity.settings;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.sebastiaan.xenopelthis.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private RecyclerView list;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_barcode_assign);
-        findGlobalViews();
+        setContentView(R.layout.activity_settings);
         setupActionBar();
-        prepareList();
-
-    }
-
-    private void findGlobalViews() {
-        list = findViewById(R.id.list);
     }
 
     private void setupActionBar() {
         Toolbar myToolbar = findViewById(R.id.settings_toolbar);
         setSupportActionBar(myToolbar);
+
         ActionBar actionbar = getSupportActionBar();
 
         if (actionbar != null) {
+            actionbar.setTitle(R.string.settings_actionbar);
             actionbar.setDisplayHomeAsUpEnabled(true);
             Drawable icon = myToolbar.getNavigationIcon();
             if (icon != null) {
                 icon.setColorFilter(getResources().getColor(R.color.colorWindowBackground, null), PorterDuff.Mode.SRC_IN);
                 myToolbar.setNavigationIcon(icon);
             }
-            actionbar.setTitle(R.string.settings_actionbar);
         }
     }
 
-    private void prepareList() {
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
-
 }
---
+++ app/src/main/java/com/sebastiaan/xenopelthis/ui/main/activity/settings/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.sebastiaan.xenopelthis.ui.main.activity.settings;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.sebastiaan.xenopelthis.R;
+
+import java.util.Locale;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        Context context = getPreferenceManager().getContext();
+        Preference language = getPreferenceManager().findPreference("language");
+        if (language != null) {
+            String currentLanguage = Locale.getDefault().getDisplayLanguage();
+            language.setSummary(context.getString(R.string.settings_fragment_current_language, currentLanguage));
+        }
+    }
+}
