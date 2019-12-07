62d449f0885675508205a6b5578a81d3d820246f
MariskaIJpelaar <m.m.j.ijpelaar@gmail.com>
2019-12-07 21:08:32 +0100

    Settings page is set up. Next step is implementing the actual functionality.

--- app/src/main/java/com/sebastiaan/xenopelthis/ui/main/activity/settings/SettingsActivity.java
+++ app/src/main/java/com/sebastiaan/xenopelthis/ui/main/activity/settings/SettingsActivity.java
@@ -1,9 +1,14 @@
 package com.sebastiaan.xenopelthis.ui.main.activity.settings;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.sebastiaan.xenopelthis.R;
 
@@ -13,7 +18,33 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager().beginTransaction().replace(R.id.settings_container, new SettingsFragment()).commit();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.settings_container, new SettingsFragment()).commit();
+        setContentView(R.layout.activity_settings);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        Toolbar myToolbar = findViewById(R.id.settings_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.settings_actionbar);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            Drawable icon = myToolbar.getNavigationIcon();
+            if (icon != null) {
+                icon.setColorFilter(getResources().getColor(R.color.colorWindowBackground, null), PorterDuff.Mode.SRC_IN);
+                myToolbar.setNavigationIcon(icon);
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
 
 }
