b76d1b0b6acdc046d27a620911cfd7a4b8cb7ff4
MariskaIJpelaar <m.m.j.ijpelaar@gmail.com>
2019-12-13 20:21:14 +0100

    export is working next step is import then perhaps sd support, but I do not know if it can be tested properly in the emulator

--- app/src/main/java/com/sebastiaan/xenopelthis/ui/main/activity/settings/SettingsFragment.java
+++ app/src/main/java/com/sebastiaan/xenopelthis/ui/main/activity/settings/SettingsFragment.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
@@ -12,13 +14,18 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import com.sebastiaan.xenopelthis.R;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-    static final int REQUEST_CODE = 0;
+    private static final int BUFFER_SIZE = 2056;
+    static final int REQUEST_CODE_EXPORT = 0;
     //TODO: perhaps retrieve from database?
     static final String DATABASE_NAME = "app.db";
 
@@ -26,15 +33,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
 
-//        Context context = getContext();
-//        context.getDatabasePath()
-
         Preference export = getPreferenceManager().findPreference("export");
         if (export != null) {
             export.setOnPreferenceClickListener(listener -> {
-                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-                //intent.type = "*/*
-                startActivityForResult(intent, REQUEST_CODE);
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT).setType("*/*");
+                startActivityForResult(intent, REQUEST_CODE_EXPORT);
                 return true;
             });
         }
@@ -44,7 +47,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         Context context = getContext();
-        if (data != null && context != null) {
+        if (requestCode == REQUEST_CODE_EXPORT && data != null && context != null) {
             Uri userChosenUri = data.getData();
             if (userChosenUri != null) {
                 try {
@@ -52,23 +55,41 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                     ContentResolver contentResolver = context.getContentResolver();
                     OutputStream outStream = contentResolver.openOutputStream(userChosenUri);
 
-                    //TODO: read and close the inStream and then outStream
-                    //then input.copyTo(output)
-                } catch (FileNotFoundException e) {
+                    copy(inStream, outStream);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
 
-
             }
-//            val inStream = getDatabasePath(DATABASE_NAME).inputStream()
-//            val outStream = contentResolver.openOutputStream(userChosenUri)
-//
-//            inStream.use { input ->
-//                    outStream.use { output ->
-//                    input.copyTo(output)
-//                }
-//            }
         }
+
+
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void copy(InputStream input, OutputStream output) throws Exception, IOException {
+        byte[] buffer = new byte[BUFFER_SIZE];
+
+        BufferedInputStream in_buf = new BufferedInputStream(input, BUFFER_SIZE);
+        BufferedOutputStream out_buf = new BufferedOutputStream(output, BUFFER_SIZE);
+
+        int n = 0;
+        try {
+            while ((n = in_buf.read(buffer, 0, BUFFER_SIZE)) != -1)
+                out_buf.write(buffer, 0, n);
+        } finally {
+            try {
+                out_buf.close();
+            } catch (IOException e) {
+                Log.e("exception: ", e.getMessage(), e);
+            }
+
+            try {
+                in_buf.close();
+            } catch (IOException e) {
+                Log.e("exception: ", e.getMessage(), e);
+            }
+
+        }
     }
 }
